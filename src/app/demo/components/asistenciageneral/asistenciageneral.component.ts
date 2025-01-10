cfe043a50cc4ce1f3f7a46943be5d23d4f79318d
juan-c <82497625+juan-caut@users.noreply.github.com>
2025-01-10 14:14:37 -0500

    asistencia general terminado con filtros

--- src/app/demo/components/asistenciageneral/asistenciageneral.component.ts
+++ src/app/demo/components/asistenciageneral/asistenciageneral.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {
     FormBuilder,
     FormGroup,
@@ -12,13 +12,16 @@ import { ButtonModule } from 'primeng/button';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
-import { TableModule } from 'primeng/table';
+import { Table, TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
 import { AsistenciaGeneralService } from '../../service/asistenciageneral.service';
 import { BreadcrumbService } from '../../service/breadcrumb.service';
 import { Asistenciageneral } from '../../model/asistenciageneral';
 import { timer } from 'rxjs';
 import { CalendarModule } from 'primeng/calendar'; // Importa el módulo de PrimeNG
+
+import * as XLSX from 'xlsx';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
     selector: 'app-asistenciageneral',
@@ -48,8 +51,8 @@ export class AsistenciageneralComponent implements OnInit {
     searchForm: FormGroup;
     editing: boolean = false;
 
-    fechainicio: string;
-    fechafin: string;
+    stardate: string;
+    enddate: string;
 
     items: any[] = [];
 
@@ -57,19 +60,77 @@ export class AsistenciageneralComponent implements OnInit {
     endDate: Date | null = null;
     minStartDate: Date | null = null;
     maxEndDate: Date | null = null;
+    filters: any;
+
+    @ViewChild('dt1') dt1: Table | undefined; // Referencia a p-table
 
     constructor(
         private mrS: AsistenciaGeneralService,
         private confirmationsService: ConfirmationService,
         private fb: FormBuilder,
         private messageService: MessageService,
-        private bS: BreadcrumbService
+        private bS: BreadcrumbService,
+        private config: PrimeNGConfig
     ) {
-        this.fechainicio = this.getStartOfMonth();
-        this.fechafin = this.getToday();
+        this.stardate = this.getStartOfMonth();
+        this.enddate = this.getToday();
+        this.startDate = new Date();
+        this.startDate.setDate(1);
+        this.endDate = new Date();
     }
 
     ngOnInit(): void {
+        this.config.setTranslation({
+            firstDayOfWeek: 1,
+            dayNames: [
+                'Domingo',
+                'Lunes',
+                'Martes',
+                'Miércoles',
+                'Jueves',
+                'Viernes',
+                'Sábado',
+            ],
+            dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+            dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+            monthNames: [
+                'Enero',
+                'Febrero',
+                'Marzo',
+                'Abril',
+                'Mayo',
+                'Junio',
+                'Julio',
+                'Agosto',
+                'Septiembre',
+                'Octubre',
+                'Noviembre',
+                'Diciembre',
+            ],
+            monthNamesShort: [
+                'Ene',
+                'Feb',
+                'Mar',
+                'Abr',
+                'May',
+                'Jun',
+                'Jul',
+                'Ago',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dic',
+            ],
+            today: 'Hoy',
+            clear: 'Limpiar',
+        });
+        this.filters = {
+            codigoEmpleado: { value: '', matchMode: 'contains' },
+            nombreEmpleado: { value: '', matchMode: 'contains' },
+            nombreMarcador: { value: '', matchMode: 'contains' },
+            // Otros filtros
+        };
+
         this.bS.setBreadcrumbs([
             { icon: 'pi pi-home', routerLink: '/Menu' },
             {
@@ -80,19 +141,17 @@ export class AsistenciageneralComponent implements OnInit {
         this.bS.currentBreadcrumbs$.subscribe((bc) => {
             this.items = bc;
         });
+
         this.loadAsistenciagenerales();
     }
 
     loadAsistenciagenerales(): void {
         console.log(
-            'Fecha inicio: ' +
-                this.fechainicio +
-                '- Fecha fin: ' +
-                this.fechafin
+            'Fecha inicio: ' + this.stardate + '- Fecha fin: ' + this.enddate
         );
         this.loading = true;
         this.mrS
-            .getAsistenciaByDateRange(this.fechainicio, this.fechafin)
+            .getAsistenciaByDateRange(this.stardate, this.enddate)
             .subscribe({
                 next: (data) => {
                     //console.log('Datos obtenidos:', data); // Console log para verificar la data
@@ -116,6 +175,60 @@ export class AsistenciageneralComponent implements OnInit {
             });
     }
 
+    generateEXCEL() {
+        const filteredData = this.dt1?.filteredValue;
+
+        if (filteredData && filteredData.length > 0) {
+            // Selecciona solo las columnas necesarias
+            const filteredColumnsData = filteredData.map((item: any) => ({
+                fecha: item.fechaFormateada,
+                hora: item.tiempoFormateado,
+                codigoEmpleado: item.codigoEmpleado,
+                nombreEmpleado: item.nombreEmpleado,
+                nombreMarcador: item.nombreMarcador,
+            }));
+
+            const wb = XLSX.utils.book_new();
+
+            // Convierte los datos filtrados (con las columnas seleccionadas) a una hoja de trabajo
+            const ws = XLSX.utils.json_to_sheet(filteredColumnsData);
+
+            // Añade la hoja de trabajo al libro de trabajo
+            XLSX.utils.book_append_sheet(wb, ws, 'Exportar');
+
+            // Descarga el archivo Excel
+            XLSX.writeFile(wb, 'Exportar.xlsx');
+        } else if (
+            this.Asistenciagenerales &&
+            this.Asistenciagenerales.length > 0
+        ) {
+            // Filtra las columnas deseadas
+            const filteredColumnsData = this.Asistenciagenerales.map(
+                (item: any) => ({
+                    fecha: item.fechaFormateada,
+                    hora: item.tiempoFormateado,
+                    codigoEmpleado: item.codigoEmpleado,
+                    nombreEmpleado: item.nombreEmpleado,
+                    nombreMarcador: item.nombreMarcador,
+                })
+            );
+
+            const wb = XLSX.utils.book_new();
+
+            // Convierte los datos filtrados (con las columnas seleccionadas) a una hoja de trabajo
+            const ws = XLSX.utils.json_to_sheet(filteredColumnsData);
+
+            // Añade la hoja de trabajo al libro de trabajo
+            XLSX.utils.book_append_sheet(wb, ws, 'Exportar');
+
+            // Descarga el archivo Excel
+            XLSX.writeFile(wb, 'Exportar.xlsx');
+        } else {
+            console.error('No hay datos para generar el archivo Excel');
+        }
+    }
+
+
     Buscar() {
         this.loading = true;
 
@@ -128,10 +241,18 @@ export class AsistenciageneralComponent implements OnInit {
                     '- Fecha fin: ' +
                     this.endDate
             );
-            this.fechainicio = this.formatDateAAAAMMDD(this.startDate);
-            this.fechafin = this.formatDateAAAAMMDD(this.endDate);
+            this.stardate = this.formatDateAAAAMMDD(this.startDate);
+            this.enddate = this.formatDateAAAAMMDD(this.endDate);
 
             this.loadAsistenciagenerales();
+        } else {
+            this.messageService.add({
+                severity: 'error',
+                summary: 'Error',
+                detail: 'Completar los campos de la fecha',
+            });
+
+            this.loading = false;
         }
     }
 
@@ -140,7 +261,7 @@ export class AsistenciageneralComponent implements OnInit {
         const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Mes en formato MM
         const day = date.getDate().toString().padStart(2, '0'); // Día en formato DD
 
-        return `${year}${month}${day}`; // Devuelve el formato AAAAMMDD
+        return `${year}${month}${day}`; //formato AAAAMMDD
     }
 
     validateDates() {
@@ -171,3 +292,17 @@ export class AsistenciageneralComponent implements OnInit {
         return `${year}${month}${day}`;
     }
 }
+/*pendiente
+
+componente asistenciageneral--
+desde dia 1 hasta la fecha actual del mes--
+
+boton con texto "buscar"---
+
+filtro por rango de fechas----
+
+
+filtro columna: nrodoc, nombre y apellidos, nombre equipo
+
+exportar
+*/
