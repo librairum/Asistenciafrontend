87a01e0c0b0214de2d97735ce35784c3ddfe6d30
Sebastian Tejeda <tejedasebastian129@gmail.com>
2025-05-22 18:34:13 -0500

    arreglo filtro

--- src/app/demo/components/horariopersonal/horariopersonal.component.ts
+++ src/app/demo/components/horariopersonal/horariopersonal.component.ts
@@ -390,7 +390,7 @@ export class HorariopersonalComponent {
             'miércoles',
             'jueves',
             'viernes',
-            'sábado',
+            'sabado',
             'domingo',
             'nombreCargo',
             'nombreDept',
